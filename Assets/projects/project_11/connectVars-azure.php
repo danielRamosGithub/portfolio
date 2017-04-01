a4b4214558374d6d9b2cb3b3bbdb2fa27c99168b
Daniel Ramos <danielramos.dcr@gmail.com>
2017-04-01 14:08:36 -0400

    changing to the correct constant variables to connect to the database

--- Assets/projects/project_11/connectVars-azure.php
+++ Assets/projects/project_11/connectVars-azure.php
@@ -1,6 +1,6 @@
 <?php
 $connectstr_dbhost = '';
-$connectstr_dbname = '';
+// $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
 
@@ -10,14 +10,14 @@ foreach ($_SERVER as $key => $value) {
 }
 
 $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-$connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+// $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
 $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
 $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
 // Define database connection constants
 define('DB_HOST', $connectstr_dbhost);
-define('DB_USER', $connectstr_dbname);
-define('DB_PASSWORD', $connectstr_dbusername);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
 define('DB_NAME', 'assignment2_iwp');
 ?>

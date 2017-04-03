619aa39f27ab4cc578a3f8a0402d0ba7f65081d5
Daniel Ramos <danielramos.dcr@gmail.com>
2017-04-03 09:40:54 -0400

    Changes to the about page and project 11

--- Assets/projects/project_11/index.php
+++ Assets/projects/project_11/index.php
@@ -1,34 +1,19 @@
 <!DOCTYPE html>
 
 <?php
-require_once('connectVars-azure.php');
-
-// $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-// if (!$link) {
-//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-//     exit;
-// }
+// require_once('connectVars-azure.php');
+require_once('connectVars-localhost.php');
 
 try {
     $conn = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME , DB_USER, DB_PASSWORD);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    // echo "Connected successfully"; 
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+    // echo "Connection failed: " . $e->getMessage();
     }
-
-// try {
-//     $sql = "INSERT INTO user (user_id, user_name, user_password) VALUES (1, 'daniel', 'daniel')";
-//     $sth = $link->query($sql);
-// } catch(PDOException $e) {
-//     echo $e->getMessage();
-// }
 
 $conn = null;
 ?>
